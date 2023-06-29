1a0df5a9861fd60c3859869ccadeab9919899372
sinchana-GR <sinchanagr2@gmail.com>
2023-06-29 21:43:14 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from app.models import *
 from django.db.models.functions import Length
+from django.db.models import Q
+
 
 # Create your views here.
 def display_topics(request):
@@ -20,8 +22,14 @@ def display_webpages(request):
     webpages=WebPage.objects.exclude(topic_name='cricket')
     webpages=WebPage.objects.order_by(Length('name'))
     webpages=WebPage.objects.order_by(Length('name').desc())
-
-
+    
+    webpages=WebPage.objects.all()
+    webpages=WebPage.objects.filter(name__in=['virat','rahul'])
+    webpages=WebPage.objects.filter(name__regex='a\w+')
+    webpages=WebPage.objects.all()
+    webpages=WebPage.objects.filter(Q(name='xyz') | Q(url__startswith='https'))
+    webpages=WebPage.objects.filter(Q(name='rahul') & Q(url__startswith='https'))
+    webpages=WebPage.objects.all()
 
     d={'webpages':webpages}
     return render(request,'display_webpages.html',d)
@@ -29,5 +37,36 @@ def display_webpages(request):
 
 def display_accessrecords(request):
     accessrecords=AccessRecord.objects.all()
+    
+    accessrecords=AccessRecord.objects.all()
+    accessrecords=AccessRecord.objects.filter(date='2023-06-22')
+    accessrecords=AccessRecord.objects.filter(date__gt='1995-01-18')
+    accessrecords=AccessRecord.objects.filter(date__lte='1995-01-18')
+    accessrecords=AccessRecord.objects.filter(date__year='2021')
+    accessrecords=AccessRecord.objects.filter(date__month='07')
+    accessrecords=AccessRecord.objects.filter(date__day='12')
+    accessrecords=AccessRecord.objects.filter(date__year__gte='2020')
+    accessrecords=AccessRecord.objects.filter(date__year__lte='2020')
+    accessrecords=AccessRecord.objects.filter(date__day__gt='12')
+    
     d={'accessrecords':accessrecords}
     return render(request,'display_accessrecords.html',d)
+
+def update_webpages(request):
+    webpages=WebPage.objects.all()
+
+    WebPage.objects.filter(name='virat').update(url='http//viratkohli.com')
+    WebPage.objects.filter(topic_name='rugby').update(url='https://pavan.com')
+    WebPage.objects.update_or_create(topic_name='foot ball',defaults={'name':'pavanwadeyar'})
+    cto=WebPage.objects.get(topic_name='cricket')
+    WebPage.objects.filter(name='Dhoni').update(topic_name='Rugby')
+    WebPage.objects.update_or_create(name='abc',defaults={'url':'http://ABCDE.com',})
+    cto=WebPage.objects.get(topic_name='cricket')
+   
+
+    d={'webpages':webpages}
+    return render(request,'display_webpages.html',d)
+    
+    
+
+    
--- project12/urls.py
+++ project12/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('display_topics/',display_topics,name='display_topics'),
     path('display_webpages/',display_webpages,name='display_webpages'),
     path('display_accessrecords/',display_accessrecords,name='display_accessrecords'),
+    path('update_webpages/',update_webpages,name='update_webpages'),
 ]
